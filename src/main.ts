2234b528cdece76d9b588aea31fe37f15eafbecb
Eaulmesse <theo.guerin01120@gmail.com>
2026-01-26 16:19:09 +0100

    add: global exceptrion filter

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Validation globale
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,           // Retire les propriétés non définies dans le DTO
-    forbidNonWhitelisted: true, // Rejette la requête si propriétés inconnues
-    transform: true,           // Transforme automatiquement les types (string -> number, etc.)
-  }));
 
-  // CORS si besoin pour le frontend
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,           
+    forbidNonWhitelisted: true, 
+    transform: true,           
+  }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   
   await app.listen(process.env.PORT ?? 3000);
